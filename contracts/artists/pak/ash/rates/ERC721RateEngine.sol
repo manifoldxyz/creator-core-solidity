9709cf9418e49afa2062d08550f31eff34b83b6e
Wilkins Chung <wilkins.chung@gmail.com>
2021-04-18 10:51:21 -0700

    Update error messages to reduce ABI size

--- contracts/artists/pak/ash/rates/ERC721RateEngine.sol
+++ contracts/artists/pak/ash/rates/ERC721RateEngine.sol
@@ -35,9 +35,9 @@ contract ERC721RateEngine is AdminControl, IERC721RateEngine {
      * @dev See {IERC721RateEngine-updateRate}.
      */
     function updateRateClass(address[] calldata contracts, uint8[] calldata rateClasses) external override adminRequired {
-        require(contracts.length == rateClasses.length, "ERC721RateEngine: Mismatched input lengths");
+        require(contracts.length == rateClasses.length, "Mismatched input lengths");
         for (uint i=0; i<contracts.length; i++) {
-            require(rateClasses[i] < 3, "ERC721RateEngine: Invalid rate class provided");
+            require(rateClasses[i] < 3, "Invalid rate class provided");
             if (_erc721ContractRateClass[contracts[i]] != rateClasses[i]) {
                 _erc721ContractRateClass[contracts[i]] = rateClasses[i];
                 emit ContractRateClassUpdate(msg.sender, contracts[i], rateClasses[i]);
@@ -49,9 +49,9 @@ contract ERC721RateEngine is AdminControl, IERC721RateEngine {
      * @dev See {IERC721RateEngine-updateRate}.
      */
     function updateRateClass(address[] calldata contracts, uint256[] calldata tokenIds, uint8[] calldata rateClasses) external override adminRequired {
-        require(contracts.length == tokenIds.length && contracts.length == rateClasses.length, "ERC721RateEngine: Mismatched input lengths");
+        require(contracts.length == tokenIds.length && contracts.length == rateClasses.length, "Mismatched input lengths");
         for (uint i=0; i<contracts.length; i++) {
-            require(rateClasses[i] < 3, "ERC721RateEngine: Invalid rate class provided");
+            require(rateClasses[i] < 3, "Invalid rate class provided");
             if (_erc721ContractTokenRateClass[contracts[i]][tokenIds[i]] != rateClasses[i]) {
                 _erc721ContractTokenRateClass[contracts[i]][tokenIds[i]] = rateClasses[i];
                 emit ContractTokenRateClassUpdate(msg.sender, contracts[i], tokenIds[i], rateClasses[i]);
@@ -63,16 +63,16 @@ contract ERC721RateEngine is AdminControl, IERC721RateEngine {
      * @dev See {INFT2ERC20RateEngine-getRate}.
      */
     function getRate(uint256 totalSupply, address tokenContract, uint256[] calldata args, string calldata spec) external view override returns (uint256) {
-        require(_enabled, "ERC721RateEngine: Disabled");
-        require(args.length == 1, "ERC721RateEngine: Invalid arguments");
-        require(keccak256(bytes(spec)) == keccak256(bytes('erc721')), "ERC721RateEngine: Only ERC721 currently supported");
+        require(_enabled, "Disabled");
+        require(args.length == 1, "Invalid arguments");
+        require(keccak256(bytes(spec)) == keccak256(bytes('erc721')), "Only ERC721 currently supported");
         uint8 rateClass;
         if (_erc721ContractTokenRateClass[tokenContract][args[0]] != 0) {
            rateClass = _erc721ContractTokenRateClass[tokenContract][args[0]];
         } else {
            rateClass = _erc721ContractRateClass[tokenContract];
         }
-        require(rateClass != 0, "ERC721RateEngine: Rate class for token not configured");
+        require(rateClass != 0, "Rate class for token not configured");
 
         uint256 m = RealMath.rpowApprox(500000000000000000, totalSupply/1000000, 100000000);
         if (rateClass == 1) {
@@ -80,7 +80,7 @@ contract ERC721RateEngine is AdminControl, IERC721RateEngine {
         } else if (rateClass == 2) {
             return RealMath.rmul(m, m)<<1;
         }
-        revert("ERC721RateEngine: Rate class for token not configured.");
+        revert("Rate class for token not configured.");
     }
 
     /**

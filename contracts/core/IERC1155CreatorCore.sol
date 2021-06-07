3b9a8029eb91701a162e5ef7ebb33d3d1cc11db5
Wilkins Chung <wilkins.chung@gmail.com>
2021-06-06 20:59:29 -0700

    update comments

--- contracts/core/IERC1155CreatorCore.sol
+++ contracts/core/IERC1155CreatorCore.sol
@@ -13,39 +13,105 @@ interface IERC1155CreatorCore is ICreatorCore {
 
     /**
      * @dev mint a token with no extension. Can only be called by an admin.
-     * Use an empty uris array if you want to use the default uri for all tokens.
-     * Use an empty uri string if you want to use the default uri for one token.
-     * Passing an array of 'to' receivers will result in generating a single new token.  Requires 0 (use default) or 1 uri.
-     *     If you pass in a single amount, every receiver will get the same amount.  If you pass in an array, it must be equal in length
-     *     to the receiver array and each receiver will get the corresponding amount of the new token.
-     * Returns tokenId minted
+     *
+     * @param to       - Can be a single element array (all tokens go to same address) or multi-element array (single token to many recipients)
+     * @param amounts  - Can be a single element array (all recipients get the same amount) or a multi-element array
+     * @param uris     - If no elements, all tokens use the default uri.
+     *                   If any element is an empty string, the corresponding token uses the default uri.
+     *
+     *
+     * Requirements: If to is a multi-element array, then uris must be empty or single element array
+     *               If to is a multi-element array, then amounts must be a single element array or a multi-element array of the same size
+     *               If to is a single element array, uris must be empty or the same length as amounts
+     *
+     * Examples:
+     *    mintBaseNew(['0x....1', '0x....2'], [1], [])
+     *        Mints a single new token, and gives 1 each to '0x....1' and '0x....2'.  Token uses default uri.
+     *    
+     *    mintBaseNew(['0x....1', '0x....2'], [1, 2], [])
+     *        Mints a single new token, and gives 1 to '0x....1' and 2 to '0x....2'.  Token uses default uri.
+     *    
+     *    mintBaseNew(['0x....1'], [1, 2], ["", "http://token2.com"])
+     *        Mints two new tokens to '0x....1'. 1 of the first token, 2 of the second.  1st token uses default uri, second uses "http://token2.com".
+     *    
+     * @return Returns list of tokenIds minted
      */
     function mintBaseNew(address[] calldata to, uint256[] calldata amounts, string[] calldata uris) external returns (uint256[] memory);
 
     /**
-     * @dev batch mint a token with no extension. Can only be called by an admin.
-     * Passing in a single element 'to' with an array of tokenIds and amounts will cause all tokens to go to the same address
-     * Passing in a single element 'tokenids' with an array of to and amounts will cause all recipients to get the same token
-     * Passing in a single element 'tokenids' and 'amounts' will cause the same amount of tokens to go to all recipients
+     * @dev batch mint existing token with no extension. Can only be called by an admin.
+     *
+     * @param to        - Can be a single element array (all tokens go to same address) or multi-element array (single token to many recipients)
+     * @param tokenIds  - Can be a single element array (all recipients get the same token) or a multi-element array
+     * @param amounts   - Can be a single element array (all recipients get the same amount) or a multi-element array
+     *
+     * Requirements: If any of the parameters are multi-element arrays, they need to be the same length as other multi-element arrays
+     *
+     * Examples:
+     *    mintBaseExisting(['0x....1', '0x....2'], [1], [10])
+     *        Mints 10 of tokenId 1 to each of '0x....1' and '0x....2'.
+     *    
+     *    mintBaseExisting(['0x....1', '0x....2'], [1, 2], [10, 20])
+     *        Mints 10 of tokenId 1 to '0x....1' and 20 of tokenId 2 to '0x....2'.
+     *    
+     *    mintBaseExisting(['0x....1'], [1, 2], [10, 20])
+     *        Mints 10 of tokenId 1 and 20 of tokenId 2 to '0x....1'.
+     *    
+     *    mintBaseExisting(['0x....1', '0x....2'], [1], [10, 20])
+     *        Mints 10 of tokenId 1 to '0x....1' and 20 of tokenId 1 to '0x....2'.
+     *    
      */
     function mintBaseExisting(address[] calldata to, uint256[] calldata tokenIds, uint256[] calldata amounts) external;
 
     /**
-     * @dev batch mint a token. Can only be called by a registered extension.
-     * Use an empty uris array if you want to use the default uri for all tokens.
-     * Use an empty uri string if you want to use the default uri for one token.
-     * Passing an array of 'to' receivers will result in generating a single new token.  Requires 0 (use default) or 1 uri.
-     *     If you pass in a single amount, every receiver will get the same amount.  If you pass in an array, it must be equal in length
-     *     to the receiver array and each receiver will get the corresponding amount of the new token.
-     * Returns tokenId minted
+     * @dev mint a token from an extension. Can only be called by a registered extension.
+     *
+     * @param to       - Can be a single element array (all tokens go to same address) or multi-element array (single token to many recipients)
+     * @param amounts  - Can be a single element array (all recipients get the same amount) or a multi-element array
+     * @param uris     - If no elements, all tokens use the default uri.
+     *                   If any element is an empty string, the corresponding token uses the default uri.
+     *
+     *
+     * Requirements: If to is a multi-element array, then uris must be empty or single element array
+     *               If to is a multi-element array, then amounts must be a single element array or a multi-element array of the same size
+     *               If to is a single element array, uris must be empty or the same length as amounts
+     *
+     * Examples:
+     *    mintExtensionNew(['0x....1', '0x....2'], [1], [])
+     *        Mints a single new token, and gives 1 each to '0x....1' and '0x....2'.  Token uses default uri.
+     *    
+     *    mintExtensionNew(['0x....1', '0x....2'], [1, 2], [])
+     *        Mints a single new token, and gives 1 to '0x....1' and 2 to '0x....2'.  Token uses default uri.
+     *    
+     *    mintExtensionNew(['0x....1'], [1, 2], ["", "http://token2.com"])
+     *        Mints two new tokens to '0x....1'. 1 of the first token, 2 of the second.  1st token uses default uri, second uses "http://token2.com".
+     *    
+     * @return Returns list of tokenIds minted
      */
     function mintExtensionNew(address[] calldata to, uint256[] calldata amounts, string[] calldata uris) external returns (uint256[] memory);
 
     /**
-     * @dev batch mint a token. Can only be called by a registered extension.
-     * Passing in a single element 'to' with an array of tokenIds and amounts will cause all tokens to go to the same address
-     * Passing in a single element 'tokenids' with an array of to and amounts will cause all recipients to get the same token
-     * Passing in a single element 'tokenids' and 'amounts' will cause the same amount of tokens to go to all recipients
+     * @dev batch mint existing token from extension. Can only be called by a registered extension.
+     *
+     * @param to        - Can be a single element array (all tokens go to same address) or multi-element array (single token to many recipients)
+     * @param tokenIds  - Can be a single element array (all recipients get the same token) or a multi-element array
+     * @param amounts   - Can be a single element array (all recipients get the same amount) or a multi-element array
+     *
+     * Requirements: If any of the parameters are multi-element arrays, they need to be the same length as other multi-element arrays
+     *
+     * Examples:
+     *    mintExtensionExisting(['0x....1', '0x....2'], [1], [10])
+     *        Mints 10 of tokenId 1 to each of '0x....1' and '0x....2'.
+     *    
+     *    mintExtensionExisting(['0x....1', '0x....2'], [1, 2], [10, 20])
+     *        Mints 10 of tokenId 1 to '0x....1' and 20 of tokenId 2 to '0x....2'.
+     *    
+     *    mintExtensionExisting(['0x....1'], [1, 2], [10, 20])
+     *        Mints 10 of tokenId 1 and 20 of tokenId 2 to '0x....1'.
+     *    
+     *    mintExtensionExisting(['0x....1', '0x....2'], [1], [10, 20])
+     *        Mints 10 of tokenId 1 to '0x....1' and 20 of tokenId 1 to '0x....2'.
+     *    
      */
     function mintExtensionExisting(address[] calldata to, uint256[] calldata tokenIds, uint256[] calldata amounts) external;
 

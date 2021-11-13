c726346495a24c6519ad3c106dbe747dad311bd2
Wilkins Chung <wilkins.chung@gmail.com>
2021-11-12 20:58:09 -0800

    add Base implementation for 721.  Add upgradeable implementation for 1155

---
+++ contracts/ERC1155CreatorUpgradeable.sol
@@ -0,0 +1,391 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @author: manifold.xyz
+
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@manifoldxyz/libraries-solidity/contracts/access/AdminControlUpgradeable.sol";
+
+import "./core/ERC1155CreatorCore.sol";
+
+/**
+ * @dev ERC1155Creator implementation (using transparent upgradeable proxy)
+ */
+contract ERC1155CreatorUpgradeable is AdminControlUpgradeable, ERC1155Upgradeable, ERC1155CreatorCore {
+
+    mapping(uint256 => uint256) private _totalSupply;
+
+    /**
+     * Initializer
+     */
+    function initialize() public initializer {
+        __ERC1155_init("");
+        __Ownable_init();
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155Upgradeable, ERC1155CreatorCore, AdminControlUpgradeable) returns (bool) {
+        return ERC1155CreatorCore.supportsInterface(interfaceId) || ERC1155Upgradeable.supportsInterface(interfaceId) || AdminControlUpgradeable.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(address, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory) internal virtual override {
+        _approveTransfer(from, to, ids, amounts);
+    }
+
+    /**
+     * @dev See {ICreatorCore-registerExtension}.
+     */
+    function registerExtension(address extension, string calldata baseURI) external override adminRequired nonBlacklistRequired(extension) {
+        _registerExtension(extension, baseURI, false);
+    }
+
+    /**
+     * @dev See {ICreatorCore-registerExtension}.
+     */
+    function registerExtension(address extension, string calldata baseURI, bool baseURIIdentical) external override adminRequired nonBlacklistRequired(extension) {
+        _registerExtension(extension, baseURI, baseURIIdentical);
+    }
+
+
+    /**
+     * @dev See {ICreatorCore-unregisterExtension}.
+     */
+    function unregisterExtension(address extension) external override adminRequired {
+        _unregisterExtension(extension);
+    }
+
+    /**
+     * @dev See {ICreatorCore-blacklistExtension}.
+     */
+    function blacklistExtension(address extension) external override adminRequired {
+        _blacklistExtension(extension);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setBaseTokenURIExtension}.
+     */
+    function setBaseTokenURIExtension(string calldata uri_) external override extensionRequired {
+        _setBaseTokenURIExtension(uri_, false);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setBaseTokenURIExtension}.
+     */
+    function setBaseTokenURIExtension(string calldata uri_, bool identical) external override extensionRequired {
+        _setBaseTokenURIExtension(uri_, identical);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setTokenURIPrefixExtension}.
+     */
+    function setTokenURIPrefixExtension(string calldata prefix) external override extensionRequired {
+        _setTokenURIPrefixExtension(prefix);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setTokenURIExtension}.
+     */
+    function setTokenURIExtension(uint256 tokenId, string calldata uri_) external override extensionRequired {
+        _setTokenURIExtension(tokenId, uri_);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setTokenURIExtension}.
+     */
+    function setTokenURIExtension(uint256[] memory tokenIds, string[] calldata uris) external override extensionRequired {
+        require(tokenIds.length == uris.length, "Invalid input");
+        for (uint i = 0; i < tokenIds.length; i++) {
+            _setTokenURIExtension(tokenIds[i], uris[i]);            
+        }
+    }
+
+    /**
+     * @dev See {ICreatorCore-setBaseTokenURI}.
+     */
+    function setBaseTokenURI(string calldata uri_) external override adminRequired {
+        _setBaseTokenURI(uri_);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setTokenURIPrefix}.
+     */
+    function setTokenURIPrefix(string calldata prefix) external override adminRequired {
+        _setTokenURIPrefix(prefix);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setTokenURI}.
+     */
+    function setTokenURI(uint256 tokenId, string calldata uri_) external override adminRequired {
+        _setTokenURI(tokenId, uri_);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setTokenURI}.
+     */
+    function setTokenURI(uint256[] memory tokenIds, string[] calldata uris) external override adminRequired {
+        require(tokenIds.length == uris.length, "Invalid input");
+        for (uint i = 0; i < tokenIds.length; i++) {
+            _setTokenURI(tokenIds[i], uris[i]);            
+        }
+    }
+
+    /**
+     * @dev See {ICreatorCore-setMintPermissions}.
+     */
+    function setMintPermissions(address extension, address permissions) external override adminRequired {
+        _setMintPermissions(extension, permissions);
+    }
+
+    /**
+     * @dev See {IERC1155CreatorCore-mintBaseNew}.
+     */
+    function mintBaseNew(address[] calldata to, uint256[] calldata amounts, string[] calldata uris) public virtual override nonReentrant adminRequired returns(uint256[] memory) {
+        return _mintNew(address(this), to, amounts, uris);
+    }
+
+    /**
+     * @dev See {IERC1155CreatorCore-mintBaseExisting}.
+     */
+    function mintBaseExisting(address[] calldata to, uint256[] calldata tokenIds, uint256[] calldata amounts) public virtual override nonReentrant adminRequired {
+        for (uint i = 0; i < tokenIds.length; i++) {
+            require(_tokensExtension[tokenIds[i]] == address(this), "A token was created by an extension");
+        }
+        _mintExisting(address(this), to, tokenIds, amounts);
+    }
+
+    /**
+     * @dev See {IERC1155CreatorCore-mintExtensionNew}.
+     */
+    function mintExtensionNew(address[] calldata to, uint256[] calldata amounts, string[] calldata uris) public virtual override nonReentrant extensionRequired returns(uint256[] memory tokenIds) {
+        return _mintNew(msg.sender, to, amounts, uris);
+    }
+
+    /**
+     * @dev See {IERC1155CreatorCore-mintExtensionExisting}.
+     */
+    function mintExtensionExisting(address[] calldata to, uint256[] calldata tokenIds, uint256[] calldata amounts) public virtual override nonReentrant extensionRequired {
+        for (uint i = 0; i < tokenIds.length; i++) {
+            require(_tokensExtension[tokenIds[i]] == address(msg.sender), "A token was not created by this extension");
+        }
+        _mintExisting(msg.sender, to, tokenIds, amounts);
+    }
+
+    /**
+     * @dev Mint new tokens
+     */
+    function _mintNew(address extension, address[] memory to, uint256[] memory amounts, string[] memory uris) internal returns(uint256[] memory tokenIds) {
+        if (to.length > 1) {
+            // Multiple receiver.  Give every receiver the same new token
+            tokenIds = new uint256[](1);
+            require(uris.length <= 1 && (amounts.length == 1 || to.length == amounts.length), "Invalid input");
+        } else {
+            // Single receiver.  Generating multiple tokens
+            tokenIds = new uint256[](amounts.length);
+            require(uris.length == 0 || amounts.length == uris.length, "Invalid input");
+        }
+
+        // Assign tokenIds
+        for (uint i = 0; i < tokenIds.length; i++) {
+            _tokenCount++;
+            tokenIds[i] = _tokenCount;
+            // Track the extension that minted the token
+            _tokensExtension[_tokenCount] = extension;
+        }
+
+        if (extension != address(this)) {
+            _checkMintPermissions(to, tokenIds, amounts);
+        }
+
+        if (to.length == 1 && tokenIds.length == 1) {
+           // Single mint
+           _mint(to[0], tokenIds[0], amounts[0], new bytes(0));
+        } else if (to.length > 1) {
+            // Multiple receivers.  Receiving the same token
+            if (amounts.length == 1) {
+                // Everyone receiving the same amount
+                for (uint i = 0; i < to.length; i++) {
+                    _mint(to[i], tokenIds[0], amounts[0], new bytes(0));
+                }
+            } else {
+                // Everyone receiving different amounts
+                for (uint i = 0; i < to.length; i++) {
+                    _mint(to[i], tokenIds[0], amounts[i], new bytes(0));
+                }
+            }
+        } else {
+            _mintBatch(to[0], tokenIds, amounts, new bytes(0));
+        }
+
+        for (uint i = 0; i < tokenIds.length; i++) {
+            if (i < uris.length && bytes(uris[i]).length > 0) {
+                _tokenURIs[tokenIds[i]] = uris[i];
+            }
+        }
+        return tokenIds;
+    }
+
+    /**
+     * @dev Mint existing tokens
+     */
+    function _mintExisting(address extension, address[] memory to, uint256[] memory tokenIds, uint256[] memory amounts) internal {
+        if (extension != address(this)) {
+            _checkMintPermissions(to, tokenIds, amounts);
+        }
+
+        if (to.length == 1 && tokenIds.length == 1 && amounts.length == 1) {
+             // Single mint
+            _mint(to[0], tokenIds[0], amounts[0], new bytes(0));            
+        } else if (to.length == 1 && tokenIds.length == amounts.length) {
+            // Batch mint to same receiver
+            _mintBatch(to[0], tokenIds, amounts, new bytes(0));
+        } else if (tokenIds.length == 1 && amounts.length == 1) {
+            // Mint of the same token/token amounts to various receivers
+            for (uint i = 0; i < to.length; i++) {
+                _mint(to[i], tokenIds[0], amounts[0], new bytes(0));
+            }
+        } else if (tokenIds.length == 1 && to.length == amounts.length) {
+            // Mint of the same token with different amounts to different receivers
+            for (uint i = 0; i < to.length; i++) {
+                _mint(to[i], tokenIds[0], amounts[i], new bytes(0));
+            }
+        } else if (to.length == tokenIds.length && to.length == amounts.length) {
+            // Mint of different tokens and different amounts to different receivers
+            for (uint i = 0; i < to.length; i++) {
+                _mint(to[i], tokenIds[i], amounts[i], new bytes(0));
+            }
+        } else {
+            revert("Invalid input");
+        }
+    }
+
+    /**
+     * @dev See {IERC1155CreatorCore-tokenExtension}.
+     */
+    function tokenExtension(uint256 tokenId) public view virtual override returns (address) {
+        return _tokenExtension(tokenId);
+    }
+
+    /**
+     * @dev See {IERC1155CreatorCore-burn}.
+     */
+    function burn(address account, uint256[] memory tokenIds, uint256[] memory amounts) public virtual override nonReentrant {
+        require(account == msg.sender || isApprovedForAll(account, msg.sender), "Caller is not owner nor approved");
+        require(tokenIds.length == amounts.length, "Invalid input");
+        if (tokenIds.length == 1) {
+            _burn(account, tokenIds[0], amounts[0]);
+        } else {
+            _burnBatch(account, tokenIds, amounts);
+        }
+        _postBurn(account, tokenIds, amounts);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setRoyalties}.
+     */
+    function setRoyalties(address payable[] calldata receivers, uint256[] calldata basisPoints) external override adminRequired {
+        _setRoyaltiesExtension(address(this), receivers, basisPoints);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setRoyalties}.
+     */
+    function setRoyalties(uint256 tokenId, address payable[] calldata receivers, uint256[] calldata basisPoints) external override adminRequired {
+        _setRoyalties(tokenId, receivers, basisPoints);
+    }
+
+    /**
+     * @dev See {ICreatorCore-setRoyaltiesExtension}.
+     */
+    function setRoyaltiesExtension(address extension, address payable[] calldata receivers, uint256[] calldata basisPoints) external override adminRequired {
+        _setRoyaltiesExtension(extension, receivers, basisPoints);
+    }
+
+    /**
+     * @dev {See ICreatorCore-getRoyalties}.
+     */
+    function getRoyalties(uint256 tokenId) external view virtual override returns (address payable[] memory, uint256[] memory) {
+        return _getRoyalties(tokenId);
+    }
+
+    /**
+     * @dev {See ICreatorCore-getFees}.
+     */
+    function getFees(uint256 tokenId) external view virtual override returns (address payable[] memory, uint256[] memory) {
+        return _getRoyalties(tokenId);
+    }
+
+    /**
+     * @dev {See ICreatorCore-getFeeRecipients}.
+     */
+    function getFeeRecipients(uint256 tokenId) external view virtual override returns (address payable[] memory) {
+        return _getRoyaltyReceivers(tokenId);
+    }
+
+    /**
+     * @dev {See ICreatorCore-getFeeBps}.
+     */
+    function getFeeBps(uint256 tokenId) external view virtual override returns (uint[] memory) {
+        return _getRoyaltyBPS(tokenId);
+    }
+    
+    /**
+     * @dev {See ICreatorCore-royaltyInfo}.
+     */
+    function royaltyInfo(uint256 tokenId, uint256 value) external view virtual override returns (address, uint256) {
+        return _getRoyaltyInfo(tokenId, value);
+    } 
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function uri(uint256 tokenId) public view virtual override returns (string memory) {
+        return _tokenURI(tokenId);
+    }
+    
+    /**
+     * @dev Total amount of tokens in with a given id.
+     */
+    function totalSupply(uint256 tokenId) external view virtual override returns (uint256) {
+        return _totalSupply[tokenId];
+    }
+
+    /**
+     * @dev See {ERC1155-_mint}.
+     */
+    function _mint(address account, uint256 id, uint256 amount, bytes memory data) internal virtual override {
+        super._mint(account, id, amount, data);
+        _totalSupply[id] += amount;
+    }
+
+    /**
+     * @dev See {ERC1155-_mintBatch}.
+     */
+    function _mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal virtual override {
+        super._mintBatch(to, ids, amounts, data);
+        for (uint256 i = 0; i < ids.length; ++i) {
+            _totalSupply[ids[i]] += amounts[i];
+        }
+    }
+
+    /**
+     * @dev See {ERC1155-_burn}.
+     */
+    function _burn(address account, uint256 id, uint256 amount) internal virtual override {
+        super._burn(account, id, amount);
+        _totalSupply[id] -= amount;
+    }
+
+    /**
+     * @dev See {ERC1155-_burnBatch}.
+     */
+    function _burnBatch(address account, uint256[] memory ids, uint256[] memory amounts) internal virtual override {
+        super._burnBatch(account, ids, amounts);
+        for (uint256 i = 0; i < ids.length; ++i) {
+            _totalSupply[ids[i]] -= amounts[i];
+        }
+    }
+}
---
+++ contracts/ERC721CreatorBase.sol
@@ -0,0 +1,345 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @author: manifold.xyz
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@manifoldxyz/libraries-solidity/contracts/access/AdminControlUpgradeable.sol";
+
+import "./core/ERC721CreatorCore.sol";
+
+/**
+ * @dev ERC721Creator implementation
+ */
+contract ERC721CreatorBase is AdminControlUpgradeable, ERC721Upgradeable, ERC721CreatorCore {
+
+    /**
+     * Initializer
+     */
+    function initialize(string memory _name, string memory _symbol) public initializer {
+        __ERC721_init(_name, _symbol);
+        __Ownable_init();
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Upgradeable, ERC721CreatorCore, AdminControlUpgradeable) returns (bool) {
+        return ERC721CreatorCore.supportsInterface(interfaceId) || ERC721Upgradeable.supportsInterface(interfaceId) || AdminControlUpgradeable.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override {
+        _approveTransfer(from, to, tokenId);    
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-registerExtension}.
+     */
+    function registerExtension(address extension, string calldata baseURI) external override adminRequired nonBlacklistRequired(extension) {
+        _registerExtension(extension, baseURI, false);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-registerExtension}.
+     */
+    function registerExtension(address extension, string calldata baseURI, bool baseURIIdentical) external override adminRequired nonBlacklistRequired(extension) {
+        _registerExtension(extension, baseURI, baseURIIdentical);
+    }
+
+
+    /**
+     * @dev See {IERC721CreatorCore-unregisterExtension}.
+     */
+    function unregisterExtension(address extension) external override adminRequired {
+        _unregisterExtension(extension);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-blacklistExtension}.
+     */
+    function blacklistExtension(address extension) external override adminRequired {
+        _blacklistExtension(extension);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setBaseTokenURIExtension}.
+     */
+    function setBaseTokenURIExtension(string calldata uri) external override extensionRequired {
+        _setBaseTokenURIExtension(uri, false);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setBaseTokenURIExtension}.
+     */
+    function setBaseTokenURIExtension(string calldata uri, bool identical) external override extensionRequired {
+        _setBaseTokenURIExtension(uri, identical);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setTokenURIPrefixExtension}.
+     */
+    function setTokenURIPrefixExtension(string calldata prefix) external override extensionRequired {
+        _setTokenURIPrefixExtension(prefix);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setTokenURIExtension}.
+     */
+    function setTokenURIExtension(uint256 tokenId, string calldata uri) external override extensionRequired {
+        _setTokenURIExtension(tokenId, uri);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setTokenURIExtension}.
+     */
+    function setTokenURIExtension(uint256[] memory tokenIds, string[] calldata uris) external override extensionRequired {
+        require(tokenIds.length == uris.length, "ERC721Creator: Invalid input");
+        for (uint i = 0; i < tokenIds.length; i++) {
+            _setTokenURIExtension(tokenIds[i], uris[i]);            
+        }
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setBaseTokenURI}.
+     */
+    function setBaseTokenURI(string calldata uri) external override adminRequired {
+        _setBaseTokenURI(uri);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setTokenURIPrefix}.
+     */
+    function setTokenURIPrefix(string calldata prefix) external override adminRequired {
+        _setTokenURIPrefix(prefix);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setTokenURI}.
+     */
+    function setTokenURI(uint256 tokenId, string calldata uri) external override adminRequired {
+        _setTokenURI(tokenId, uri);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setTokenURI}.
+     */
+    function setTokenURI(uint256[] memory tokenIds, string[] calldata uris) external override adminRequired {
+        require(tokenIds.length == uris.length, "ERC721Creator: Invalid input");
+        for (uint i = 0; i < tokenIds.length; i++) {
+            _setTokenURI(tokenIds[i], uris[i]);            
+        }
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setMintPermissions}.
+     */
+    function setMintPermissions(address extension, address permissions) external override adminRequired {
+        _setMintPermissions(extension, permissions);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-mintBase}.
+     */
+    function mintBase(address to) public virtual override nonReentrant adminRequired returns(uint256) {
+        return _mintBase(to, "");
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-mintBase}.
+     */
+    function mintBase(address to, string calldata uri) public virtual override nonReentrant adminRequired returns(uint256) {
+        return _mintBase(to, uri);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-mintBaseBatch}.
+     */
+    function mintBaseBatch(address to, uint16 count) public virtual override nonReentrant adminRequired returns(uint256[] memory tokenIds) {
+        tokenIds = new uint256[](count);
+        for (uint16 i = 0; i < count; i++) {
+            tokenIds[i] = _mintBase(to, "");
+        }
+        return tokenIds;
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-mintBaseBatch}.
+     */
+    function mintBaseBatch(address to, string[] calldata uris) public virtual override nonReentrant adminRequired returns(uint256[] memory tokenIds) {
+        tokenIds = new uint256[](uris.length);
+        for (uint i = 0; i < uris.length; i++) {
+            tokenIds[i] = _mintBase(to, uris[i]);
+        }
+        return tokenIds;
+    }
+
+    /**
+     * @dev Mint token with no extension
+     */
+    function _mintBase(address to, string memory uri) internal virtual returns(uint256 tokenId) {
+        _tokenCount++;
+        tokenId = _tokenCount;
+
+        // Track the extension that minted the token
+        _tokensExtension[tokenId] = address(this);
+
+        _safeMint(to, tokenId);
+
+        if (bytes(uri).length > 0) {
+            _tokenURIs[tokenId] = uri;
+        }
+
+        // Call post mint
+        _postMintBase(to, tokenId);
+        return tokenId;
+    }
+
+
+    /**
+     * @dev See {IERC721CreatorCore-mintExtension}.
+     */
+    function mintExtension(address to) public virtual override nonReentrant extensionRequired returns(uint256) {
+        return _mintExtension(to, "");
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-mintExtension}.
+     */
+    function mintExtension(address to, string calldata uri) public virtual override nonReentrant extensionRequired returns(uint256) {
+        return _mintExtension(to, uri);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-mintExtensionBatch}.
+     */
+    function mintExtensionBatch(address to, uint16 count) public virtual override nonReentrant extensionRequired returns(uint256[] memory tokenIds) {
+        tokenIds = new uint256[](count);
+        for (uint16 i = 0; i < count; i++) {
+            tokenIds[i] = _mintExtension(to, "");
+        }
+        return tokenIds;
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-mintExtensionBatch}.
+     */
+    function mintExtensionBatch(address to, string[] calldata uris) public virtual override nonReentrant extensionRequired returns(uint256[] memory tokenIds) {
+        tokenIds = new uint256[](uris.length);
+        for (uint i = 0; i < uris.length; i++) {
+            tokenIds[i] = _mintExtension(to, uris[i]);
+        }
+    }
+    
+    /**
+     * @dev Mint token via extension
+     */
+    function _mintExtension(address to, string memory uri) internal virtual returns(uint256 tokenId) {
+        _tokenCount++;
+        tokenId = _tokenCount;
+
+        _checkMintPermissions(to, tokenId);
+
+        // Track the extension that minted the token
+        _tokensExtension[tokenId] = msg.sender;
+
+        _safeMint(to, tokenId);
+
+        if (bytes(uri).length > 0) {
+            _tokenURIs[tokenId] = uri;
+        }
+        
+        // Call post mint
+        _postMintExtension(to, tokenId);
+        return tokenId;
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-tokenExtension}.
+     */
+    function tokenExtension(uint256 tokenId) public view virtual override returns (address) {
+        require(_exists(tokenId), "Nonexistent token");
+        return _tokenExtension(tokenId);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-burn}.
+     */
+    function burn(uint256 tokenId) public virtual override nonReentrant {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721Creator: caller is not owner nor approved");
+        address owner = ownerOf(tokenId);
+        _burn(tokenId);
+        _postBurn(owner, tokenId);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setRoyalties}.
+     */
+    function setRoyalties(address payable[] calldata receivers, uint256[] calldata basisPoints) external override adminRequired {
+        _setRoyaltiesExtension(address(this), receivers, basisPoints);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setRoyalties}.
+     */
+    function setRoyalties(uint256 tokenId, address payable[] calldata receivers, uint256[] calldata basisPoints) external override adminRequired {
+        require(_exists(tokenId), "Nonexistent token");
+        _setRoyalties(tokenId, receivers, basisPoints);
+    }
+
+    /**
+     * @dev See {IERC721CreatorCore-setRoyaltiesExtension}.
+     */
+    function setRoyaltiesExtension(address extension, address payable[] calldata receivers, uint256[] calldata basisPoints) external override adminRequired {
+        _setRoyaltiesExtension(extension, receivers, basisPoints);
+    }
+
+    /**
+     * @dev {See IERC721CreatorCore-getRoyalties}.
+     */
+    function getRoyalties(uint256 tokenId) external view virtual override returns (address payable[] memory, uint256[] memory) {
+        require(_exists(tokenId), "Nonexistent token");
+        return _getRoyalties(tokenId);
+    }
+
+    /**
+     * @dev {See IERC721CreatorCore-getFees}.
+     */
+    function getFees(uint256 tokenId) external view virtual override returns (address payable[] memory, uint256[] memory) {
+        require(_exists(tokenId), "Nonexistent token");
+        return _getRoyalties(tokenId);
+    }
+
+    /**
+     * @dev {See IERC721CreatorCore-getFeeRecipients}.
+     */
+    function getFeeRecipients(uint256 tokenId) external view virtual override returns (address payable[] memory) {
+        require(_exists(tokenId), "Nonexistent token");
+        return _getRoyaltyReceivers(tokenId);
+    }
+
+    /**
+     * @dev {See IERC721CreatorCore-getFeeBps}.
+     */
+    function getFeeBps(uint256 tokenId) external view virtual override returns (uint[] memory) {
+        require(_exists(tokenId), "Nonexistent token");
+        return _getRoyaltyBPS(tokenId);
+    }
+    
+    /**
+     * @dev {See IERC721CreatorCore-royaltyInfo}.
+     */
+    function royaltyInfo(uint256 tokenId, uint256 value) external view virtual override returns (address, uint256) {
+        require(_exists(tokenId), "Nonexistent token");
+        return _getRoyaltyInfo(tokenId, value);
+    } 
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "Nonexistent token");
+        return _tokenURI(tokenId);
+    }
+    
+}

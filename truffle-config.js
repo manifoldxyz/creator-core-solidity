a5d7d808ac709546b5d13a0d06811ef0f8e232af
Wilkins Chung <wilkins.chung@gmail.com>
2022-11-22 13:20:35 -0800

    increase optimizer runs

--- truffle-config.js
+++ truffle-config.js
@@ -93,7 +93,7 @@ module.exports = {
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 100
+          runs: 150
         },
       //  evmVersion: "byzantium"
       },

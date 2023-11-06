6045388d54903d8002535025c6e4baf5f8649158
Wilkins Chung <wilkins.chung@gmail.com>
2023-11-06 14:04:56 -0800

    add deployment proxy

---
+++ contracts/DeploymentProxy.yul
@@ -0,0 +1,28 @@
+object "DeploymentProxy" {
+	// deployment code
+	code {
+		let size := datasize("runtime")
+		datacopy(0, dataoffset("runtime"), size)
+		return(0, size)
+	}
+	object "runtime" {
+		// deployed code
+		code {
+      // Copy bytecode without salt into position 0
+			calldatacopy(0, 32, sub(calldatasize(), 32))
+      // Create2, using the bytecode stored in memory from the prior line, and loading the salt from calldata
+			let result := create2(callvalue(), 0, sub(calldatasize(), 32), calldataload(0))
+			if iszero(result) { revert(0, 0) }
+      // Store function selector for transferOwnership(address)
+      mstore(0, 0xf2fde38b)
+			// Store the caller
+			mstore(add(0, 0x20), caller())
+			// Call transferOwnership(caller())
+      let transfer := call(gas(), result, 0, 28, 36, 0, 0)
+      if iszero(transfer) { revert(0, 0) }
+      // Store address
+			mstore(0, result)
+			return(12, 20)
+		}
+	}
+}
---
+++ test/proxy/DeploymentProxy.t.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import {ERC721CreatorImplementation} from "creator-core/ERC721CreatorImplementation.sol";
+import {ERC165Checker} from "openzeppelin/utils/introspection/ERC165Checker.sol";
+import {Strings} from "openzeppelin/utils/Strings.sol";
+import {Test} from "forge-std/Test.sol";
+import {Proxy} from "openzeppelin/proxy/Proxy.sol";
+import {Address} from "openzeppelin/utils/Address.sol";
+import {StorageSlot} from "openzeppelin/utils/StorageSlot.sol";
+import "./lib/YulDeployer.sol";
+import "forge-std/console.sol";
+
+contract ProxyMock is Proxy {
+    constructor(address impl) {
+        assert(_IMPLEMENTATION_SLOT == bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1));
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = impl;
+        (bool success, ) = impl.delegatecall(abi.encodeWithSignature("initialize(string,string)", "Test Proxy", "TP"));
+        require(success, "Initialization failed");
+    }
+        
+    /**
+     * @dev Storage slot with the address of the current implementation.
+     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
+     * validated in the constructor.
+     */
+    bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    /**
+     * @dev Returns the current implementation address.
+     */
+    function implementation() public view returns (address) {
+        return _implementation();
+    }
+
+    function _implementation() internal override view returns (address) {
+        return StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value;
+    }    
+}
+
+interface DeploymentProxy {}
+
+contract DeploymentProxyTest is Test {
+    DeploymentProxy proxy;
+    ProxyMock proxyMock;
+    address implementation;
+
+    YulDeployer yulDeployer = new YulDeployer();
+    address creator = address(0xC12EA7012);
+
+    function setUp() public virtual {
+        proxy = DeploymentProxy(yulDeployer.deployContract("contracts/DeploymentProxy.yul"));
+        implementation = address(new ERC721CreatorImplementation());
+    }
+
+    function testDeploy() public {
+        vm.prank(creator);
+        bytes32 salt = 0x0;
+        bytes memory bytecode = abi.encodePacked(salt, vm.getCode("DeploymentProxy.t.sol:ProxyMock"), abi.encode(implementation));
+        (bool success, bytes memory data) = address(proxy).call(bytecode);
+        assertTrue(success);
+        address deployedAddress = address(uint160(bytes20(data)));
+        address contractOwner = ERC721CreatorImplementation(deployedAddress).owner();
+        assertEq(creator, contractOwner);
+    }
+}
